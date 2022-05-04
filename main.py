ff236f849cf6f42682d5a19d8763d26d101fa9b4
Juan E. Gil <96081036+JuanE-Gil@users.noreply.github.com>
2022-05-04 05:21:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+user_ = "abcd"
+passw_ = "1234"
+print("Bienvenido ingrese sus datos")
+print("Usuario: ")
+user = input()
+print("Contraseña: ")
+passw = input()
+
+if user==user_ and passw==passw_:
+  print("Bienvenido al ")
+else:
+  print("Usuario y Contraseña Inválido")
+#esto se guarda solo no? si
+
+
+clave = "contraseña"
+contraseña =""
+while contraseña != clave:
+  contraseña = input("Ingrese la contraseña: ")
+print("contraseña correcta")
+
+
+#mñana seguiremos nn quiero un poco de cod nn y despues a mimir nn
+# termila y mejoralo
+
+# yo tengo que hacer la tarea de comunicacion :(
